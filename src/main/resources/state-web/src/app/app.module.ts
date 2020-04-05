afa3e716c29997f1b2ac79f9d8f118720423d7af
João Paulo Santarém <joao.santarem@dbccompany.com.br>
2020-04-05 17:52:09 -0300

    Add bootstrap and sb-admin template

--- src/main/resources/state-web/src/app/app.module.ts
+++ src/main/resources/state-web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './container/app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +17,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
