0b6a4fcb20d48cc21fbaaac07e61f98f9670f52b
João Paulo Santarém <joao.santarem@dbccompany.com.br>
2020-04-05 17:18:56 -0300

    Add state-web-app

--- src/main/resources/state-web/e2e/src/app.e2e-spec.ts
+++ src/main/resources/state-web/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('estado-web app is running!');
+    expect(page.getTitleText()).toEqual('state-web app is running!');
   });
 
   afterEach(async () => {
--- src/main/resources/state-web/karma.conf.js
+++ src/main/resources/state-web/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/estado-web'),
+      dir: require('path').join(__dirname, './coverage/state-web'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
