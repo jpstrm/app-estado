a65bcfbadd1f275f20d4e3ab7ab8831d1928c4f3
João Paulo Santarém <joao.santarem@dbccompany.com.br>
2020-04-06 22:22:56 -0300

    Fix data script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,14 @@
 -- Insert initial data
 
-INSERT INTO estado
-  (id, name, code, created_at, flag_url)
-  VALUES(1, 'Rio Grande do Sul', 'RS', '2020-04-05 10:00:00.320000000',
+INSERT INTO estado (id, name, code, created_at, flag_url)
+  VALUES(1, 'Rio Grande do Sul', 'RS', '2020-04-05 10:00:00',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Bandeira_do_Rio_Grande_do_Sul.svg/2000px-Bandeira_do_Rio_Grande_do_Sul.svg.png'),
-  (2, 'Santa Catarina', 'SC', '2020-04-05 10:00:00.320000000',
+  (2, 'Santa Catarina', 'SC', '2020-04-05 10:00:00',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Bandeira_de_Santa_Catarina.svg/1200px-Bandeira_de_Santa_Catarina.svg.png'),
-  (3, 'Paraná', 'PR', '2020-04-05 10:00:00.320000000',
+  (3, 'Paraná', 'PR', '2020-04-05 10:00:00',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Bandeira_do_Paran%C3%A1.svg/1200px-Bandeira_do_Paran%C3%A1.svg.png');
 
-INSERT INTO cidade
-  (id, name, population, state_id, created_at)
-  VALUES(1, 'Porto Alegre', '49999', 1, '2020-04-05 10:00:00.320000000'),
-  (2, 'Florianópolis', '50000', 2, '2020-04-05 10:00:00.320000000'),
-  (3, 'Curitiba', '50001', 3, '2020-04-05 10:00:00.320000000');
+INSERT INTO cidade (id, name, population, state_id, created_at)
+  VALUES(1, 'Porto Alegre', '49999', 1, '2020-04-05 10:00:00'),
+  (2, 'Florianópolis', '50000', 2, '2020-04-05 10:00:00'),
+  (3, 'Curitiba', '50001', 3, '2020-04-05 10:00:00');
